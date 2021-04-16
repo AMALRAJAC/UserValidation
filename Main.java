85937181817d67ca8dc4ee18d35a2c5651f1df23
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-16 09:39:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,6 +21,67 @@ class regex
             }
         }
     }
+    public static void password_check(String password){
+        for(int i=0;!valid;i++) {
+
+            if (password.length() > 8) {
+                System.out.println("password length >8");
+                if (password.matches(".*[a-z].*")) {
+                    System.out.println("password contain at least one lowercase  character");
+                    if (password.matches(".*[A-Z].*")) {
+                        System.out.println("password contain at least one uppercase  character");
+                        if (password.matches(".*[0-9].*")) {
+                            System.out.println("password contain at least one numerical character");
+                            if (password.matches(".*[*.!@#$%^&(){}:;'<>,?/~`_+=|].*")) {
+                                Pattern p = Pattern.compile("[*.!@#$%^&(){}:;'<>,?/~`_+=|]");
+                                Matcher m = p.matcher(password);
+                                int count = 0;
+                                while (m.find()) {
+                                    count++;
+                                }
+                                System.out.println("Count of special characters present= "+count);
+                                if (count == 1) {
+                                    System.out.println("password contain exact one special character");
+                                    valid=true;
+                                } else {
+                                    Scanner sc=new Scanner(System.in);
+                                    System.out.println("String is not acceptable");
+                                    System.out.println("Re-enter the value...");
+                                    password=sc.nextLine();
+                                }
+                            }else{
+                                Scanner sc=new Scanner(System.in);
+                                System.out.println("String is not acceptable");
+                                System.out.println("Re-enter the value...");
+                                password=sc.nextLine();
+                            }
+                        }else{
+                            Scanner sc=new Scanner(System.in);
+                            System.out.println("String is not acceptable");
+                            System.out.println("Re-enter the value...");
+                            password=sc.nextLine();
+                        }
+                    }else{
+                        Scanner sc=new Scanner(System.in);
+                        System.out.println("String is not acceptable");
+                        System.out.println("Re-enter the value...");
+                        password=sc.nextLine();
+                    }
+                }else{
+                    Scanner sc=new Scanner(System.in);
+                    System.out.println("String is not acceptable");
+                    System.out.println("Re-enter the value...");
+                    password=sc.nextLine();
+                }
+            }else{
+                Scanner sc=new Scanner(System.in);
+                System.out.println("String is not acceptable");
+                System.out.println("Re-enter the value...");
+                password=sc.nextLine();
+            }
+
+        }
+    }
     public static void firstName() {
         String regex = "^[A-Z][a-z]{2,10}$";
         Scanner sc=new Scanner(System.in);
@@ -30,8 +91,45 @@ class regex
         isValid(firstName,regex);
         valid=false;
     }
+    public static void secondName() {
+        String regex = "^[A-Z][a-z]{2,10}$";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the second name");
+        System.out.println("1.First letter should be capital\n2.Name should contain minimum of 3 letters");
+        String secondName = sc.nextLine();
+        isValid(secondName,regex);
+        valid=false;
+    }
+    public static void emailId() {
+        String regex = "^abc.[A-Za-z0-9]+@bl.co[A-Za-z0-9.]$";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the email id");
+        String emailId = sc.nextLine();
+        isValid(emailId,regex);
+        valid=false;
+    }
+    public static void contactNumber() {
+        String regex = "^([0-9]{2}){1}+\s[0-9]{10}$";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the phone number");
+        System.out.println("Country code <space> phone number ");
+        String contactNumber = sc.nextLine();
+        isValid(contactNumber,regex);
+        valid=false;
+    }
+    public static void password() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the password");
+        String password = sc.nextLine();
+        password_check(password);
+        valid=false;
+    }
     public static void main(String[] args)
     {
        firstName();
+       secondName();
+       emailId();
+       contactNumber();
+       password();
     }
 }
